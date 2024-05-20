8f5d017d306f6bfab5b8753886ef2f1f9b7c84a1
A-EL 에이엘 <a-el@A-ELui-MacBookAir.local>
2024-05-20 17:43:42 +0900

    Modified ALLOWED_HOSTS

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8818waxrznfe+**+1y-8ncoy##n0hy+n7(dz8mmovj!pr9m5%b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
